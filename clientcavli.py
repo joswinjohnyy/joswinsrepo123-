a6e6fa14e197dd8a0e3d5648566b7d9f2ccc9cca
joswin johny <johnyjoswin8@gmail.com>
2025-07-08 08:50:44 +0530

    new branch is commited

--- clientcavli.py
+++ clientcavli.py
@@ -4,7 +4,35 @@ import requests
 resp = requests.get("http://127.0.0.1:5000/greet")
 print("GET Response:", resp.json())
 
-# Send POST
+# ssend POST
 payload = {"sensor": "temperature", "value": 26}
 resp = requests.post("http://127.0.0.1:5000/echo", json=payload)
 print("POST Response:", resp.json())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
